4793c370375a326dd35ba1f645f6e4415879efcc
NipunBhat <bhatnipun29@gmail.com>
2023-08-27 21:02:46 +0530

    Status bar implemented

--- TkImageViewer/ImageViewerGui.py
+++ TkImageViewer/ImageViewerGui.py
@@ -4,7 +4,7 @@ import os
 from pathlib import Path
 
 root = Tk()
-root.title('Image Viewer')
+root.title('Codemy.com Image Viewer')
 root.iconbitmap(Path(r"C:\Users\Nipun Bhat\Documents\TKApps\IconFiles\Python.ico"))
 
 imgdirPath = Path(r"C:\Users\Nipun Bhat\Documents\TKApps\Images")
@@ -14,6 +14,12 @@ for indx,path in enumerate(image_list):
     image_list[indx] = ImageTk.PhotoImage(Image.open(tempPath))
 
 my_label = Label(image=image_list[0])
+my_label.grid(row=0, column=0, columnspan=3)
+
+
+status = Label(root, text="Image 1 of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+my_label = Label(image=image_list[0])
+
 my_label.grid(row=0, column=0, columnspan=3)
 
 def forward(image_number):
@@ -33,6 +39,11 @@ def forward(image_number):
 	button_back.grid(row=1, column=0)
 	button_forward.grid(row=1, column=2)
 
+	status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+	status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+	
+		
+
 def back(image_number):
 	global my_label
 	global button_forward
@@ -50,8 +61,10 @@ def back(image_number):
 	button_back.grid(row=1, column=0)
 	button_forward.grid(row=1, column=2)
 
-
-
+	# Update Status Bar
+	status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+	status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+	
 button_back = Button(root, text="<<", command=back, state=DISABLED)
 button_exit = Button(root, text="Exit Program", command=root.quit)
 button_forward = Button(root, text=">>", command=lambda: forward(2))
@@ -59,6 +72,8 @@ button_forward = Button(root, text=">>", command=lambda: forward(2))
 
 button_back.grid(row=1, column=0)
 button_exit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
+button_forward.grid(row=1, column=2, pady=10)
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
 
 root.mainloop()
