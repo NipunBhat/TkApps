dd3766503a94cfe0cbe393919a87bf2cb132dd83
NipunBhat <bhatnipun29@gmail.com>
2023-08-27 21:08:00 +0530

    fixed spacing issues

--- TkImageViewer/ImageViewerGui.py
+++ TkImageViewer/ImageViewerGui.py
@@ -16,7 +16,6 @@ for indx,path in enumerate(image_list):
 my_label = Label(image=image_list[0])
 my_label.grid(row=0, column=0, columnspan=3)
 
-
 status = Label(root, text="Image 1 of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
 my_label = Label(image=image_list[0])
 
@@ -42,8 +41,6 @@ def forward(image_number):
 	status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
 	status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 	
-		
-
 def back(image_number):
 	global my_label
 	global button_forward
@@ -69,11 +66,9 @@ button_back = Button(root, text="<<", command=back, state=DISABLED)
 button_exit = Button(root, text="Exit Program", command=root.quit)
 button_forward = Button(root, text=">>", command=lambda: forward(2))
 
-
 button_back.grid(row=1, column=0)
 button_exit.grid(row=1, column=1)
 button_forward.grid(row=1, column=2, pady=10)
 status.grid(row=2, column=0, columnspan=3, sticky=W+E)
-
 
 root.mainloop()
